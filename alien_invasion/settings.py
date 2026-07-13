cd2ab64c6d7bd204b494d4c5742fad693b0e2a75
abdulrehmancs <abd.rehman.cs@gmail.com>
2026-07-13 17:20:42 +0500

    Changed the screen resolution and aadded the Fullscreen ability to game and added left moving option as well, limited the ship movement to prevent ship  disappearing from the edges

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,5 +4,9 @@ class settings:
     def __init__(self):
         """Initialize the game's settings."""
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = (230,230,230)
+        
+        
+        #ship settings
+        self.ship_speed = 1.5
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -4,8 +4,9 @@ class Ship:
     """A class to manage the ship."""
     
     def __init__(self, ai_game):
-        """Initoialize the ship and set its starting position."""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         #load the ship image and get its rect.
@@ -14,13 +15,23 @@ class Ship:
         
         self.rect.midbottom = self.screen_rect.midbottom
         
+        #store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+        
         #movement flag; start with the ship that's not moving.
         self.moving_right = False
+        self.moving_left = False
         
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
+        #Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            
+        #Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
