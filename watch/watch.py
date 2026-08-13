53e4f26a8bae7ed8d4469afc5b693db187db23b0
abdulrehmancs <abd.rehman.cs@gmail.com>
2026-08-13 20:03:19 +0500

    written a simple Python program that takes user input and matches it against predefined cases using the match-case statement. The program responds with specific messages based on the input, including greetings and house names from the Harry Potter series.

--- watch/watch.py
+++ watch/watch.py
@@ -1,6 +1,14 @@
-import re
-
-um = input("Enter your text: ").strip()
+match_in = input("Enter your text: ").strip().lower()
 
 
-print(len(re.findall(r"um", um)))
+match match_in:
+    case "slytherin":
+        print("You have slytherin..You are going to be a great wizard but may not be a good person.")
+    case "gryffindor":
+        print("You have gryffindor..You are brave and courageous.")
+    case "hufflepuff":
+        print("You have hufflepuff..You are loyal and hardworking.")
+    case "ravenclaw":
+        print("You have ravenclaw..You are intelligent and wise.")
+    case _:
+        print("You have entered an invalid house name.")
