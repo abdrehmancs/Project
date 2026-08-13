bba3170956514f807778486ea0953b64b116f080
abdulrehmancs <abd.rehman.cs@gmail.com>
2026-08-13 23:09:46 +0500

    written a simple Python program that takes user input and matches it against predefined cases using the match-case statement. The program responds with specific messages based on the input, including greetings and house names from the Harry Potter series.

--- watch/watch.py
+++ watch/watch.py
@@ -1,14 +1,25 @@
-match_in = input("Enter your text: ").strip().lower()
+import random
 
+class House:
+    def __init__(self):
+        self.name = input("Enter your name in hogwarts: ")
+        self.house = random.choice(["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"])
+        
+        print(f"Welcome {self.name}! You have been sorted into {self.house}.")
+        
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, value):
+        if value not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house. Please choose from Gryffindor, Hufflepuff, Ravenclaw, or Slytherin.")
+        self._house = value    
+        
+        
+        
+def main():
+    student = House()
 
-match match_in:
-    case "slytherin":
-        print("You have slytherin..You are going to be a great wizard but may not be a good person.")
-    case "gryffindor":
-        print("You have gryffindor..You are brave and courageous.")
-    case "hufflepuff":
-        print("You have hufflepuff..You are loyal and hardworking.")
-    case "ravenclaw":
-        print("You have ravenclaw..You are intelligent and wise.")
-    case _:
-        print("You have entered an invalid house name.")
+main()
