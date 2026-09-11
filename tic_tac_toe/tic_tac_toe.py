fca69cece9d73fc74dbf93bb34307f6d31981dc4
abdrehmancs <abd.rehman.cs@gmail.com>
2026-09-11 03:42:55 -0700

    This is the tic tac toe game simulation

---
+++ tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,20 @@
+import random
+
+players=['X']
+
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+
+
+board[0] = 'X'
+
+print("\033c", end="")
+
+
+print("---+---+---")
+print(f" {board[0]} | {board[1]} | {board[2]} ")
+print("---+---+---")
+print(f" {board[3]} | {board[4]} | {board[5]} ")
+print("---+---+---")
+print(f" {board[6]} | {board[7]} | {board[8]} ")
+print("---+---+---")
+
