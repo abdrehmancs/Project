c3f9e3cd9bc9f5112ef0e8364c8fb49294451b87
abdrehmancs <abd.rehman.cs@gmail.com>
2026-09-15 07:13:21 -0700

    This is tic-tac-toe game simulation

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,3 +1,5 @@
+import sys
+
 print('')
 print('')
 print('')
@@ -36,19 +38,89 @@ def print_borad():
 
 
 
-print("\033c", end="")
 
 print_borad()
 
 turn = 1
 
 for _ in range(len(board)):
-    mark = 0
+    X_choice = 0
     if turn % 2 == 0:
         print("Player X's Turn: ")
-        print("Enter the position of marking...")
+        choice = int(input("Enter the position of marking..."))
+        board[choice] = 'X'
+
+        if board[0] == board[1] == board[2] == 'X':
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[0] == board[3] == board[6] == 'X':
+            print("Player X has won   ")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[0] == board[4] == board[8] == 'X':
+            print("Player X has won   ")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[5] == board[8] == 'X':
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[4] == board[6] == 'X' : 
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[5] == board[8] == 'X':
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()    
+        elif board[1] == board[4] == board[7] == 'X':
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[3] == board[4] == board[5] == 'X':
+            print("Player X has won")
+            input("Press Enter to continue")
+            sys.exit()
+
     else:
         print("Player O's Turn: ")
-        print("Enter the position of marking...")
+        choice = int(input("Enter the position of marking..."))
+        board[choice] = 'O'
+        if board[0] == board[1] == board[2] == 'O':
+            print("Player X has won")
 
+        elif board[0] == board[3] == board[6] == 'O':                
+            print("Player O has won   ")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[0] == board[4] == board[8] == 'O':
+            print("Player O has won   ")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[5] == board[8] == 'O':
+            print("Player O has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[4] == board[6] == 'O':
+            print("Player O has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[2] == board[5] == board[8] == 'O':
+            print("Player O has won")  
+            input("Press Enter to continue")  
+            sys.exit()
+        elif board[1] == board[4] == board[7] == 'O':
+            print("Player O has won")
+            input("Press Enter to continue")
+            sys.exit()
+        elif board[3] == board[4] == board[5] == 'O':
+            print("Player O has won")
+            input("Press Enter to continue")
+            sys.exit()
+    print("\033c", end="")
+    print_borad()
     turn += 1
+
+
+
