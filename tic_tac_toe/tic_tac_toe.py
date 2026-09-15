3944309c3614f4db164658442d8731d3b80f15c6
abdrehmancs <abd.rehman.cs@gmail.com>
2026-09-14 23:47:52 -0700

    This is tic-tac-toe implementation in CLI

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,20 +1,54 @@
-import random
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
 
-players=['X']
+print("=================    Welcome     ============")
+print("=================    to the      ============")
+print("=================      Tic       ============")
+print("=================      Tac       ============")
+print("=================      Toe       ============")
+print("=================      Game      ============")
+
+print('')
+print('')
+print('')
+print('')
+print('')
 
 board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+players=['X','O']
+
+def print_borad():
+    print("---+---+---")
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+    print("---+---+---")
 
 
-board[0] = 'X'
+
+
 
 print("\033c", end="")
 
+print_borad()
 
-print("---+---+---")
-print(f" {board[0]} | {board[1]} | {board[2]} ")
-print("---+---+---")
-print(f" {board[3]} | {board[4]} | {board[5]} ")
-print("---+---+---")
-print(f" {board[6]} | {board[7]} | {board[8]} ")
-print("---+---+---")
+turn = 1
 
+for _ in range(len(board)):
+    mark = 0
+    if turn % 2 == 0:
+        print("Player X's Turn: ")
+        print("Enter the position of marking...")
+    else:
+        print("Player O's Turn: ")
+        print("Enter the position of marking...")
+
+    turn += 1
